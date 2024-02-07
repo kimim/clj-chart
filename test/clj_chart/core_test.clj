9d1bcafc8e4a784d5b62d243bf25073f8ca5eb31
kimim <kimi.im@outlook.com>
2024-02-07 20:53:46 +0800

    update test cases

--- test/clj_chart/core_test.clj
+++ test/clj_chart/core_test.clj
@@ -3,14 +3,20 @@
     [clojure.test :refer :all]
     [clj-chart.core :as c]))
 
+(defn save-check-delete [chart]
+  (let [img "chart.svg"]
+    (c/save! img chart)
+    (is (clojure.string/includes? (slurp img) "viewBox"))
+    (clojure.java.io/delete-file img)))
+
 (deftest quick-test
-  (testing "FIXME, I fail."
+  (testing "create quick chart."
+    (let [x [1.0 2.0 3.0]
+          y [1.0 4.0 9.0]]
+      (save-check-delete (c/quick-xy x y)))
+
     (let [ds [[1.0 1.0][2.0 3.0]]]
-      (c/save!
-       "quick-seq.svg"
-       (c/quick-seq ds)))
+      (save-check-delete (c/quick-seq ds)))
+
     (let [ds [{:x 1.0 :y 1.0} {:x 5.0 :y 1.0} {:x 2.0 :y 3.0}]]
-      (c/save!
-       "quick.svg"
-       (c/quick-mapseq ds)))
-    (is (= 1 1))))
+      (save-check-delete (c/quick-mapseq ds)))))
